7cb07b4757d14f26dc85c8cd75ce9c392f7e090f
Italo Santos <italorsants@gmail.com>
2022-06-11 20:37:02 -0300

    mudanças

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import os
 
 app = Flask(__name__)
 
-csrf = CSRFProtect(app)
+csrf = CSRFProtect(app)  
 
 @app.route("/")
 def pagina_inicial():
     return "Hello World from Italo 7ASO"
 
-
 if __name__ == '__main__':
     port = os.getenv('PORT')
     app.run('0.0.0.0', port=port)
+    
